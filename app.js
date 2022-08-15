c5c463fd3e9a48af92f79fe1ea09ba8372de538b
jawad <jawad.mabtoul@laposte.net>
2022-08-15 20:49:49 +0200

    update

--- app.js
+++ app.js
@@ -2,9 +2,43 @@ const btn = document.querySelector(".btn");
 const menu = document.querySelector(".menu");
 let ouvert = document.querySelector(".btn_ouvert");
 let fermer = document.querySelector(".btn_fermer");
-console.log(fermer);
-
+const title = document.querySelector(".principal");
+const p = document.querySelector("p");
+const arrow = document.querySelector(".arrow");
 menu.style.display = "none";
+
+window.addEventListener("load", function () {
+  const TL = gsap.timeline({ paused: true });
+
+  TL.staggerFrom(
+    title,
+    2,
+    { transform: "scale(0)", height: 0, opacity: 0, ease: "power2.out" },
+    0.3
+  )
+    .staggerFrom(
+      p,
+      2,
+      { transform: "scale(0)", opacity: 0, ease: "power2.out" },
+      0.3,
+      "-=1"
+    )
+    .staggerFrom(
+      arrow,
+      2,
+      { left: 50, opacity: 0, transform: "scale(0)", ease: "power2.out" },
+      0.3,
+      "-=1"
+    )
+    .staggerFrom(
+      btn,
+      2,
+      { transform: "scale(0)", ease: "power2.out" },
+      0.3,
+      "-=1"
+    );
+  TL.play();
+});
 
 fermer.style.display = "none";
 btn.addEventListener("click", function () {
