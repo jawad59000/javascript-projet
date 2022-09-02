5748bb55c663321fb691749c5479652dc77a23b1
jawad <jawad.mabtoul@laposte.net>
2022-09-02 23:18:28 +0200

    update

--- 22- Pagination/displaySingleUser.js
+++ 22- Pagination/displaySingleUser.js
@@ -1,23 +1,17 @@
 export default async function displayDrink(user) {
   //   console.log(user);
-  const {
-    avatar_url: image,
-    html_url: link,
-    login,
-    name,
-    id,
-    events_url: events,
-  } = user;
+  const { avatar_url: image, html_url: link, login, name, id, location } = user;
+  console.log(user);
   const showUserNoName = `
              <img
                 src="${image}"
               />
               <div class="info">
               <h2> <span>Sorry i don't have a name</span> </h2><br>
-              <h3>My pseudo is :  <span>${login}</span> </h3>
-              <h3>Link to my GitHub : <span>${link}</span> </h3>
               <h3>My id : <span>${id}</span> </h3>
-              <h3>Link to my events : <span>${events}</span> </h3>
+              <h3>My pseudo is :  <span>${login}</span> </h3>
+              <h3>My address : <span>Sorry i don't have an address to</span> </h3>
+              <h3>Link to my GitHub : <span><a href="${link}">Clique pour voir mont GitHub</a></span> </h3>
 
                 <a hreF="./index.html"><button>ALL Users</button></a>
               </div>
@@ -32,10 +26,10 @@ export default async function displayDrink(user) {
               />
               <div class="info">
               <h2> <span>${name}</span> </h2><br>
-              <h3>My pseudo is :  <span>${login}</span> </h3>
-              <h3>Link to my GitHub : <span>${link}</span> </h3>
               <h3>My id : <span>${id}</span> </h3>
-              <h3>Link to my events : <span>${events}</span> </h3>
+              <h3>My pseudo is :  <span>${login}</span> </h3>
+              <h3>My address : <span>${location}</span> </h3>
+              <h3>Link to my GitHub : <span><a href="${link}">Clique pour voir mont GitHub</a></span> </h3>
 
                 <a hreF="./index.html"><button>ALL Users</button></a>
               </div>
